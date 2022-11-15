bcda2ff618a756c66b888bfd7122b8279cee7e5c
Theneau Maxime <theneau.maxime@gmail.com>
2022-11-15 16:56:55 +0100

    install next.js

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>
+          test
+        </div>
+      </main>
+      
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
