14def8a99955c0a7a5c199e71d1f3baf89c88929
Theneau Maxime <theneau.maxime@gmail.com>
2022-11-29 15:39:41 +0100

    feat(Contact): created call api post form

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,10 +1,22 @@
-import { useState } from 'react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-export default function Contact() {
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:8000/api/pages/Contact');
+  const pageContact = await res.json();
+
+  return { props: { pageContact } };
+}
+
+
+export default function Contact({ pageContact }) {
   const [state, setState] = useState({
-    name: '',
-    email: '',
-    message: '',
+    form: {
+      contactTo: 'assoc',
+      name: '',
+      email: '',
+      message: '',
+    },
   });
 
   const handleSubmit = (e) => {
@@ -14,54 +26,80 @@ export default function Contact() {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(state.form),
     };
-    fetch('http://localhost:8000/api/message', requestOptions)
+    fetch('http://localhost:8000/api/contact', requestOptions)
+      .catch((err) => console.log(err))
       .finally(() => {
         setState({
-          ...state,
           form: {
+            contactTo: 'assoc',
             name: '',
             email: '',
-            subject: '',
             message: '',
           },
         });
       })
-      .catch((err) => console.log(err));
   };
+
   return (
     <div>
-      <h1>Contact</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="input">
-          Nom
-          <input
-            type="text"
-            name="name"
-            value={state.form.name}
-            required
-          />
-        </div>
-        <div className="input">
-          Email
-          <input
-            type="email"
-            required
-          />
-        </div>
-        <div className="input">
-          <select>
-            <option value="assoc">Kréa Tout Eure</option>
-            <option value="Webmaster">Webmaster</option>
-          </select>
-        </div>
-        <div className="textarea">
-          Message
-          <textarea
-            name="message"
-            required
-          />
-        </div>
-      </form>
+      <header>
+        <Image
+          src={pageContact.imgHeader.path}
+          alt={pageContact.title}
+          width={pageContact.imgHeader.width}
+          height={pageContact.imgHeader.height}
+        />
+        <h1>{pageContact.title}</h1>
+      </header>
+      <main>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input">
+            Nom
+            <input
+              type="text"
+              name="name"
+              value={state.form.name}
+              onChange={(e) => setState({ ...state, form: { ...state.form, name: e.target.value } })}
+              required
+            />
+          </div>
+          <div className="input">
+            Email
+            <input
+              type="email"
+              name="email"
+              value={state.form.email}
+              onChange={(e) => setState({ ...state, form: { ...state.form, email: e.target.value } })}
+              required
+            />
+          </div>
+          <div className="input">
+            <select
+              onChange={(e) => setState({
+                ...state,
+                form: { ...state.form, contactTo: e.target.value },
+              })}
+            >
+              <option value="assoc">Kréa Tout Eure</option>
+              <option value="webmaster">Webmaster</option>
+            </select>
+          </div>
+          <div className="textarea">
+            Message
+            <textarea
+              value={state.form.message}
+              onChange={(e) => setState({
+                ...state,
+                form: { ...state.form, message: e.target.value },
+              })}
+              name="message"
+              required
+            />
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
+      </main>
     </div>
   );
 }
