3f031ee8e39a1a47144710368b1afe3ca8d323ef
Theneau Maxime <theneau.maxime@gmail.com>
2024-10-21 15:19:21 +0200

    feat: change link footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       Site réalisé par
-      <Link href="https://theneau-maxime.fr" target="_blank"> Maxime Theneau</Link>
+      <Link href="https://theneaumaxime.fr" target="_blank"> Maxime Theneau</Link>
     </footer>
   );
 }
