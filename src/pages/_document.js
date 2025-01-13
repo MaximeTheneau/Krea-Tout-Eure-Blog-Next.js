82786a5ee0146be87cbac8e8c1d787108beebb4e
Theneau Maxime <theneau.maxime@gmail.com>
2025-01-13 13:39:20 +0100

    Feat: add gg webmaster

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
   return (
     <Html lang="fr">
       <Head>
+        <meta name="google-site-verification" content="yjpJTbMBL6qRubkSPGcEbVb2boBL2gPGzE1YOY-TyuU" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body>
