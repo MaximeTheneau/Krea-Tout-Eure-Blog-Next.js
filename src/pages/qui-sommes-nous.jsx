efa733f300e4b0aae79360d6456accea46f89065
Theneau Maxime <theneau.maxime@gmail.com>
2025-01-13 13:19:20 +0100

    Feat: change document.js

--- src/pages/qui-sommes-nous.jsx
+++ src/pages/qui-sommes-nous.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import styles from '../styles/Pages.module.scss';
+import Link from 'next/link';
 
 export async function getStaticProps() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/pages/qui-sommes-nous`);
@@ -26,7 +27,8 @@ export default function QuiSommesNous({ pageAbout }) {
         <meta property="og:site_name" content="https://kreatouteure.fr" />
         <meta property="og:image" content={pageAbout.imgHeader.path} />
       </Head>
-      <header className={`${styles.pages__header} card`}>
+      <main className={`${styles.pages__main} card`}>
+      <section className={`${styles.pages__header} `}>
         <h1>{pageAbout.title}</h1>
         <Image
           src={pageAbout.imgHeader.path}
@@ -36,10 +38,34 @@ export default function QuiSommesNous({ pageAbout }) {
           style={{ width: 'auto', height: 'auto' }}
           priority
         />
-      </header>
-      <main className={`${styles.pages__main} card`}>
+      </section>
         <h2>{pageAbout.subtitle}</h2>
         <p>{pageAbout.contents}</p>
+         <section>
+    <h2>Notre mission</h2>
+    <p>
+      L’objectif de Kréa Tout Eure est de faire découvrir le tricot et d’en faire une activité accessible à tous. Nous organisons régulièrement des ateliers, des événements et des rencontres afin de permettre à nos membres de partager leurs connaissances, leurs techniques et de se soutenir mutuellement dans leurs projets.    
+    </p>
+      </section>
+
+      <section>
+        <h2>Pourquoi le tricot ?</h2>
+        <p>
+          Le tricot est bien plus qu’un simple passe-temps : c’est un véritable art qui permet de créer de belles choses tout en favorisant la relaxation et en prenant soin de soi. C’est également un moyen puissant de se connecter aux autres et de faire partie d’une communauté bienveillante, solidaire et créative.
+        </p>
+      </section>
+
+      <section>
+        <h2>Rejoignez-nous</h2>
+        <p>
+  Que vous soyez débutant(e) ou expert(e), nous vous invitons à rejoindre Kréa Tout Eure. Venez partager votre passion du tricot, apprendre de nouvelles techniques, et faire partie d'une communauté dynamique et accueillante.
+        </p>
+        <Link
+                href="/contact"
+              >
+                <button type="button" className="button-glass">Contactez-nous</button>
+              </Link>
+      </section>
       </main>
     </>
   );
